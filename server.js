a72fd776d060f79ee97206860a26724b5db0576d
ees-ja perekonnanimi <svetik24@hotmail.com>
2024-04-28 16:09:05 +0300

    week9-Favouritemovie

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.static("public"));
+
+app.get("/", (request, response)=>{
+    
+response.sendFile(__dirname + "/index.html");
+
+ });
+
+app.get("/about", (request, response) =>{
+    response.send("Hello, I am Svetlana. Nice to meet you");
+});
+
+app.listen(3000, ()=>{ 
+console.log("Server is running on Port 3000.");
+
+
+});
